827fa62bb6ff9112500ae953e620dc21b6a9d778
Andrew Ge-Hall <ge-hall@outlook.com>
2022-12-01 07:43:53 +1000

    chore:init project

---
+++ data.py
@@ -0,0 +1,17 @@
+
+header_line = '--------------------'
+def get_data_from_file(filename):
+    with open(filename, "r") as f:
+        data = f.read().rstrip()
+    print(header_line)
+    print(f"reading from file {filename}")
+    print(data)
+    print(header_line)
+    return data
+
+
+def get_data_from_file_as_int_list(filename):
+    data = get_data_from_file(filename)
+    data = [int(x) for x in data.split(',')]
+    return data
+
