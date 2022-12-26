4d80bc2085c81dcfb10d8dc9ecad22cbd3bbc361
Andrew Ge-Hall <ge-hall@outlook.com>
2022-12-26 12:30:44 +1000

    feat: commit to test new neovim setup ;)

---
+++ day14/day.py
@@ -0,0 +1,30 @@
+from data import get_data_from_file, get_data_from_file_as_int_list, header_line
+
+DAY = 100
+data = get_data_from_file(f"day{DAY}.input")
+print(f'transformed data for solution')
+print(data)
+print(header_line)
+
+def solve_part1():
+    print(header_line)
+    print(f'solution part 1')
+
+    # solution code
+
+
+    print(header_line)
+
+def solve_part2():
+    print(header_line)
+    print(f'solution part 2')
+    # solution code
+
+    print()
+    print(f'result = {X}')
+    print(header_line)
+
+
+if __name__ == '__main__':
+    solve_part1()
+    # solve_part2()
