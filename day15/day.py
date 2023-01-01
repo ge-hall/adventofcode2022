2b3038cd81818ab12b6be2542571287a51585010
Andrew Ge-Hall <ge-hall@outlook.com>
2023-01-01 22:53:59 +1000

    WOrking on Adjacency Matrix, it is built but need to apply min spanning tree as max

--- day15/day.py
+++ day15/day.py
@@ -1,3 +1,4 @@
+from os import wait
 from data import get_data_from_file, get_data_from_file_as_int_list, header_line
 import re
 import numpy as np
@@ -87,11 +88,88 @@ def solve_part2():
     print(header_line)
     print(f"solution part 2")
     # solution code
+    # get sensor and beacon data
+    sensors = []
+    for line in data.splitlines():
+        sensor_beacon_pair = extract_numbers_from_string(line)
+        sensors.append(sensor_beacon_pair)
+    sensor_distances = []
+    for sensor in sensors:
+        x1 = sensor[0]
+        y1 = sensor[1]
+        x2 = sensor[2]
+        y2 = sensor[3]
+        # get manhattan distance from sensor to beacon
+        distance = abs(x1 - x2) + abs(y1 - y2)
+        sensor_distances.append([x1, y1, distance])
+    found = False
+    for x in range(0, 4000000):
+        for y in range(0, 4000000):
+            in_range = 0
+            if y % 100000 == 0:
+                print(x, y)
+            for sensor in sensor_distances:
+                line_distance = abs(sensor[0] - x) + abs(sensor[1] - y)
+                if line_distance <= sensor[2]:
+                    in_range += 1
+            if in_range == 0:
+                print(
+                    f"point ({x},{y}) is not in range of all sensors with in_range {in_range}"
+                )
+                print(4000000 * x + y)
+                found = True
+                break
+        if found:
+            break
 
-    print()
     print(header_line)
 
 
+def solve_part2_1():
+    # solution code
+    # get sensor and beacon data
+    sensors = []
+    for line in data.splitlines():
+        sensor_beacon_pair = extract_numbers_from_string(line)
+        sensors.append(sensor_beacon_pair)
+    sensor_distances = []
+    for sensor in sensors:
+        x1 = sensor[0]
+        y1 = sensor[1]
+        x2 = sensor[2]
+        y2 = sensor[3]
+        # get manhattan distance from sensor to beacon
+        distance = abs(x1 - x2) + abs(y1 - y2)
+        sensor_distances.append([x1, y1, distance])
+    LINES = 4000000
+    for line in range(0, LINES):
+        ranges = []
+        print(line)
+        for sensor in sensor_distances:
+            x1 = sensor[0]
+            y1 = sensor[1]
+            distance = sensor[2]
+            # check if line is in range of sensor
+            # build range of sensor over line
+            # check if line is in range of sensor
+            if sensor[1] - sensor[2] <= line <= sensor[1] + sensor[2]:
+                # get x range of sensor over this line
+                ranges.append(
+                    [
+                        sensor[2] - abs(line - sensor[1]),
+                        sensor[2] + abs(line - sensor[1]),
+                    ]
+                )
+        # merge ranges
+        for r1 in ranges:
+            for r2 in ranges:
+                if r1[0] <= r2[0] <= r1[1] or r1[0] <= r2[1] <= r1[1]:
+                    r1[0] = min(r1[0], r2[0])
+                    r1[1] = max(r1[1], r2[1])
+                    ranges.remove(r2)
+        print(f"line {line} ranges {ranges}")
+
+
 if __name__ == "__main__":
-    solve_part1()
-    # solve_part2()
+    # solve_part1()
+    solve_part2_1()
--- day16/day.py
+++ day16/day.py
@@ -37,45 +37,133 @@ valve_names = [
     "ZZ",
 ]
 
-max_recursion_level = 2
-open_valves = []
-skip_valves = []
+max_recursion_level = 6
+results = []
 
 # need to track all the open valves
-def moveToValve(valve, minutes, recursion_level):
+def moveToValve(
+    valve: list,
+    discovered_valves: list,
+    remaining_minutes: int,
+    pressure_released: int,
+    recursion_level: int,
+):
+    """
+    Resursive function to traverse the tunnel system
+    keep track of remaining_minutes
+    discovered valves should not be traversed again
+    @param valve: the valve we are currently at
+    @param discovered_valves: the valves we have discovered so far
+    @param remaining_minutes: the remaining minutes we have to traverse the tunnel system
+    @param pressure_released: the pressure released so far
+    @param recursion_level: the current recursion level
+    @return (remaining_minutes, pressure_released)
+
+    """
     if recursion_level > max_recursion_level:
         return
 
-    if minutes <= 0:
+    if remaining_minutes <= 0:
+        results.append(pressure_released)
         return
+    # cost of traversing to this valve  = 1 minute
+    remaining_minutes -= 1
+    discovered_valves.append(valve[1])
 
-    current_pressure = sum([int(valve[-1]) for valve in open_valves])
-    print(f" valves {open_valves} are open, releasing {current_pressure} pressure")
+    print(f"valve = {valve}")
+    print(f"discovered_valves = {discovered_valves}")
+    # cost of opening this valve = 1 minute
+    if valve[-1] > 0:
 
-    for valve_spec in valves:
-        if valve_spec[1] == valve:
-            if valve_spec[-1] > 0:
-                # open valve if closed
-                if valve_spec[0] == False:
-                    print(f"open valve {valve_spec[1]}")
-                    valve_spec[0] = True
-                    open_valves.append(valve_spec)
-                    minutes -= 1
-            else:
-                skip_valves.append(valve_spec[1])
-            for to_valve in valve_spec[2:-1]:
-                if (
-                    to_valve in [valve[1] for valve in open_valves]
-                    or to_valve in skip_valves
-                ):
-                    continue
-                print(f"move from {valve} To Valve {to_valve}")
-                moveToValve(
-                    to_valve,
-                    minutes - 1,
-                    recursion_level + 1,
-                )
-    return
+        pressure_released += valve[-1] * remaining_minutes
+        print(f"pressure_released by this valve: {valve[-1]}")
+        print(
+            f"eventual pressure released: {pressure_released} with {remaining_minutes} minutes remaining"
+        )
+        remaining_minutes -= 1
+
+    print()
+    child_valves = sorted(
+        [v for v in valves if v[1] in valve[2:-1] and v[1] not in discovered_valves],
+        key=lambda x: -x[-1],
+    )
+
+    print(f"child_valves = {child_valves}")
+    for child_valve in child_valves:
+        if child_valve[1] in discovered_valves:
+            continue
+
+        moveToValve(
+            child_valve,
+            discovered_valves,
+            remaining_minutes,
+            pressure_released,
+            recursion_level + 1,
+        )
+
+    return (remaining_minutes, pressure_released)
+
+
+# maximum cost matrix with limit on number of edges and nodes to traverse
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v, w):
+        self.graph[u].append((v, w))
+
+    def printArr(self, dist):
+        print("Vertex Distance from Source")
+        for i in range(self.V):
+            print("{0}\t\t{1}".format(i, dist[i]))
+
+    def buildAdjacencyMatrix(self):
+        matrix = [[0 for x in range(self.V)] for y in range(self.V)]
+        print(f"matrix = {matrix}")
+        for u in self.graph:
+            print(f"u = {u}")
+            for v, w in self.graph[u]:
+                print(f"u = {u}, v = {v}, w = {w}")
+                _u = get_index_from_valve_name(u)
+                v = get_index_from_valve_name(v)
+                print(f"u = {_u}, v = {v}, w = {w}")
+                matrix[_u][v] = w
+        for row in matrix:
+            print(row)
+
+    def maxCostMatrix(self):
+        self.buildAdjacencyMatrix()
+        dist = [float("Inf")] * self.V
+        dist[0] = 0
+        print(f"len(V) = {self.V}")
+        for _ in range(self.V - 1):
+            for u in range(self.V):
+                for v, w in self.graph[u]:
+                    print(f"u = {u}, v = {v}, w = {w}")
+                    if dist[u] != float("Inf") and dist[u] + w > dist[v]:
+                        dist[v] = dist[u] + w
+
+        for u in range(self.V):
+            for v, w in self.graph[u]:
+                print(f"u = {u}, v = {v}, w = {w}")
+                if dist[u] != float("Inf") and dist[u] + w > dist[v]:
+                    print("Graph contains negative weight cycle")
+                    return
+
+        self.printArr(dist)
+
+    def printGraph(self):
+        print(self.graph)
+
+
+def get_index_from_valve_name(valve_name: str):
+    print(valve_name)
+    print([v[0] for v in valves if v[1] == valve_name])
+    return [v[0] for v in valves if v[1] == valve_name][0]
 
 
 def solve_part1():
@@ -85,10 +173,10 @@ def solve_part1():
     # solution code
 
     # Valve AA has flow rate=0; tunnels lead to Valves DD II BB
-    for line in data.splitlines():
+    for i, line in enumerate(data.splitlines()):
         print(line)
         valve = []
-        valve.append(False)
+        valve.append(i)
         rate = 0
         for word in line.split(" "):
             if word[-1] == ",":
@@ -100,11 +188,22 @@ def solve_part1():
         valve.append(rate)
         valves.append(valve)
     print(valves)
-
-    minutes = 30
+    print(header_line)
     # traverse each possible tunnel path. calculate the pressure at the end of each path
     # get path with minimum pressure
-    moveToValve("AA", minutes, 0)
+    # set first valve as discovered
+    # result = moveToValve(valves[0], [], 30, 0, 0)
+
+    # get number of vertices from valves
+    g = Graph(len(valves))
+
+    for valve in valves:
+        for v in valve[2:-1]:
+            rate = [vi for vi in valves if vi[1] == v][0][-1]
+            g.addEdge(valve[1], v, rate)
+
+    g.printGraph()
+    g.maxCostMatrix()
 
     print(header_line)
 
@@ -115,7 +214,6 @@ def solve_part2():
     # solution code
     print()
 
-    print(f"result = {X}")
     print(header_line)
 
 
